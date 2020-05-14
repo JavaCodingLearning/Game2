df13346ac5c0149bddcefa5fdbed0f084a246e6b
JavaCodingLearning <developertoday123@gmail.com>
2020-05-14 13:51:07 +0100

    add a health bar

--- newGame/src/main/java/main/Health.java
+++ newGame/src/main/java/main/Health.java
@@ -9,19 +9,23 @@ import java.awt.Graphics;
  * @author coding_java
  */
 public class Health {
-    
+    //be able to transition from green to red 
     public static int health = 100;
+    //rgb value go from 0 which is black to 255
+    private int GreenVal = 255;
     public void tick(){
         health = Game.clamp(health, 1, 100);
+        GreenVal = Game.clamp(GreenVal, 0, 255);
+        GreenVal = health*2;
     }
     
     public void render(Graphics g ){
         g.setColor(Color.black);
-        g.fillRect(15, 15, 200, 32);
-        g.setColor(Color.red);
-        g.fillRect(15, 15, health * 2, 32);
+        g.fillRect(10, 10, 200, 25);
+        g.setColor(new Color(75, GreenVal, 0));
+        g.fillRect(10, 10, health * 2, 25);
         g.setColor(Color.white);
-        g.drawRect(15, 15, 200, 32);
+        g.drawRect(10, 10, 200, 25);
     }
     
 }
